4bd9680dfd519d76a55aa39f8612664ef4248d64
Unknown <barkerbg001@gmail.com>
2019-09-09 15:39:28 +0200

    Added Weapon Input Script to Player Script

--- Shooting Game/Assets/Scripts/Player.cs
+++ Shooting Game/Assets/Scripts/Player.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof (PlayerController))]
+[RequireComponent (typeof (GunController))]
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5;
 
     Camera viewCamera;
     PlayerController controller;
+    GunController gunController;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
         viewCamera = Camera.main;
     }
 
@@ -37,5 +40,8 @@ public class Player : MonoBehaviour
         }
 
         // Weapon input
+        if (Input.GetMouseButton(0)){
+            gunController.Shoot();
+        }
     }
 }
