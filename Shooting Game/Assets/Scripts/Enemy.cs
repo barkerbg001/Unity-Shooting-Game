76bea916e5c9de9476ac0df9a3760df76e5cd60e
Unknown <barkerbg001@gmail.com>
2019-09-09 17:27:54 +0200

    Started Enemy AI (Might not Work)

--- Shooting Game/Assets/Scripts/Enemy.cs
+++ Shooting Game/Assets/Scripts/Enemy.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
+    UnityEngine.AI.NavMeshAgent pathfinder;
+    Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     // Update is called once per frame
