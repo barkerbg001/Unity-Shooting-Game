3b0eb76d9bf4a6c336b36979fdcf16421ebba2ea
Unknown <barkerbg001@gmail.com>
2019-09-10 19:32:35 +0200

    Added Enemy Damage Script

--- Shooting Game/Assets/Scripts/IDamageable.cs
+++ Shooting Game/Assets/Scripts/IDamageable.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IDamageable : MonoBehaviour
+public interface IDamageable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void TakeHit (float damage, RaycastHit)
 }
