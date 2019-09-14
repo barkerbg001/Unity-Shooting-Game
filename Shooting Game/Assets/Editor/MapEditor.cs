5c6bfd5acf2d898e52cb798240f58f8270b07bf4
Unknown <barkerbg001@gmail.com>
2019-09-14 10:13:18 +0200

    Fixed Error

--- Shooting Game/Assets/Editor/MapEditor.cs
+++ Shooting Game/Assets/Editor/MapEditor.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class MapEditor : MonoBehaviour
+[CustomEditor (typeof (MapGenerator))]
+public class MapEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public override void OnInspectorGUI (){
+        base.OnInspectorGUI();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        MapGenerator map = target as MapGenerator;
+
+        map.GererateMap();
     }
 }
