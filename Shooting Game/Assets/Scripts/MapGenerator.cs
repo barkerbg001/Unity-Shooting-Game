e75fb00e4dedd560af0d8c4f37e03946fddfaf8b
Unknown <barkerbg001@gmail.com>
2019-09-14 09:21:22 +0200

    Started Map Generator

---
+++ Shooting Game/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+
+    public void GererateMap(){
+        
+    }
+}
